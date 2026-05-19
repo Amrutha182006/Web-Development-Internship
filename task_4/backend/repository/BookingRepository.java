7861d2e53d581e037801a202cf7eb83debfd419f
S Amrutha <samru212006@gmail.com>
2026-05-19 15:20:31 +0530

    commit

--- task_4/backend/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.emeralddynasty.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.emeralddynasty.entity.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-
-}
